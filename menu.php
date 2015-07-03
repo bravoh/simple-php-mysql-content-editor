f3a5dd1290040022ecd75b438f333463979684d3
bravoh <kiptoobravo@gmail.com>
2015-07-03 12:10:10 +0200

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,12 @@
+<a href="./"><h1>News and Events</h1></a>
+<ul id='adminmenu'>
+	<li><a href='index.php'>News Preview</a></li>
+	<li><a href='add-news.php'>Post News Item</a></li>
+	<li><a href='events.php'>Events Preview</a></li>
+	<li><a href='add-event.php'>Add an Event</a></li>
+	<li><a href="../">Admin Home</a></li>
+	<li><a href='#'>Help</a></li>
+	<li><a href="http://www.kitalepolytechnic.ac.ke/" target="_blank">Go to Website</a></li>
+</ul>
+<div class='clear'></div>
+<hr />
